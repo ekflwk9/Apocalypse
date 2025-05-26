fdafe2d5947954ebdc3f6fc32c17e3545a2f62de
ekflwk9 <ekflwk9@naver.com>
2025-05-27 02:53:56 +0900

    [feat] Ui 기본틀 설계 및 Helper 스크립트 생성
    shaderUI 구현(Ui 전용 글로벌 볼륨 효과)
    FadeIn & Out 구현
    Helper 스크립트 생성 (공통적인 간단한 유틸 메서드)
    DebugHelper 스크립트 생성 (간단한 에디터 전용 스크립트)

--- Assets/01.Scripts/Ui/Status/StatusUi.cs
+++ Assets/01.Scripts/Ui/Status/StatusUi.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatusUi : MonoBehaviour
 {
-    Dictionary<UiCode, GameObject> window = new Dictionary<UiCode, GameObject>();
+    private Dictionary<UiCode, GameObject> window = new Dictionary<UiCode, GameObject>();
 
     private Slot[] inventorySlot;
     private Slot[] storageSlot;
@@ -11,25 +12,39 @@ public class StatusUi : MonoBehaviour
 
     private void Awake()
     {
-        
+        Find("Inventory", out statusSlot);
+        Find("Storage", out statusSlot);
+        Find("PlayerInfo", out statusSlot);
     }
 
-    private void FindInventorySlot(string _parentName)
+    private void Find(string _parentName, out Slot[] _slotArray)
     {
         var parent = Helper.FindChild(this.transform, _parentName);
 
-    }
+        if (Enum.TryParse<UiCode>(_parentName, out var type))
+        {
+            window.Add(type, parent.gameObject);
+        }
 
-    private void ASD()
-    {
+        else
+        {
+            DebugHelper.Log($"{_parentName}이라는 타입의 UiCode가 존재하지 않음");
+            _slotArray = null;
+            return;
+        }
+
+        var childCount = parent.childCount;
         var tempList = new List<Slot>();
 
-        for (int i = 0; i < inventorySlot.Length; i++)
+        for (int i = 0; i < childCount; i++)
         {
-
+            var child = parent.GetChild(i);
+            if (child.TryGetComponent<Slot>(out var target)) tempList.Add(target);
         }
-    }
 
+        _slotArray = new Slot[tempList.Count];
+        tempList.CopyTo(0, _slotArray, 0, tempList.Count);
+    }
 
     /// <summary>
     /// 켜져있을 경우 꺼짐 / 꺼져있을 경우 켜짐
@@ -49,7 +64,6 @@ public class StatusUi : MonoBehaviour
         }
     }
 
-   
     /// <summary>
     /// Ui 활성화 상태를 직접 지정함
     /// </summary>
--- Assets/01.Scripts/Ui/UiManager.cs
+++ Assets/01.Scripts/Ui/UiManager.cs
@@ -4,9 +4,9 @@ public enum UiCode
 {
     None = 0,
     Inventory = 1,
-    Storage = 1,
-    PlayerInfo = 1,
-    Farming = 1,
+    Storage = 2,
+    PlayerInfo = 3,
+    Farming = 4,
 }
 
 public class UiManager : MonoBehaviour
@@ -15,19 +15,28 @@ public class UiManager : MonoBehaviour
     public MenuUi menu { get; private set; }
     public StatusUi status { get; private set; }
     public DragImage drag { get; private set; }
+    public ShaderUi shader { get; private set; }
+    public Fade fade { get; private set; }
 
     private void Awake()
     {
         if (UiManager.instance == null)
         {
-            menu = this.GetComponentInChildren<MenuUi>();
+            //true = 비활성화 오브젝트도 검색
+            menu = this.GetComponentInChildren<MenuUi>(true); 
             if (menu == null) DebugHelper.Log($"{this.name}에 MenuUi스크립트가 있는 자식 오브젝트가 존재하지 않음");
 
-            status = this.GetComponentInChildren<StatusUi>();
+            status = this.GetComponentInChildren<StatusUi>(true);
             if (status == null) DebugHelper.Log($"{this.name}에 StatusUi스크립트가 있는 자식 오브젝트가 존재하지 않음");
 
-            drag = this.GetComponentInChildren<DragImage>();
+            drag = this.GetComponentInChildren<DragImage>(true);
             if (drag == null) DebugHelper.Log($"{this.name}에 DragImage스크립트가 있는 자식 오브젝트가 존재하지 않음");
+
+            fade = this.GetComponentInChildren<Fade>(true);
+            if (fade == null) DebugHelper.Log($"{this.name}에 Fade스크립트가 있는 자식 오브젝트가 존재하지 않음");
+
+            shader = this.GetComponentInChildren<ShaderUi>(true);
+            if (shader == null) DebugHelper.Log($"{this.name}에 ShaderUi스크립트가 있는 자식 오브젝트가 존재하지 않음");
 
             instance = this;
             DontDestroyOnLoad(this.gameObject);
