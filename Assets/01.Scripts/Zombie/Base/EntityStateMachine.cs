067b790e9abb84e8d4358046165131d33354134b
성민 유 <usm9401@gmail.com>
2025-05-26 15:19:51 +0900

    [feat]Monster 시스템 생성

---
+++ Assets/01.Scripts/Zombie/Base/EntityStateMachine.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+
+
+public class StateStruct
+{
+    Entity entity;
+    protected Dictionary<EntityEnum, EntityState> StateDictionary = new Dictionary<EntityEnum, EntityState>();
+    public EntityState CurrentState { get; protected set; }
+
+    public virtual void Init()
+    {
+        foreach (KeyValuePair<EntityEnum, EntityState> state in StateDictionary)
+        {
+            state.Value.SetOwner(entity);
+        }
+    }
+
+    public void SetOwner(Entity _Entity)
+    {
+        entity = _Entity;
+    }
+
+    public void SetState(EntityState _State)
+    {
+        CurrentState = _State;
+    }
+
+    public virtual void SetState(EntityEnum _State)
+    {
+        CurrentState = StateDictionary[_State];
+    }
+}
+
+public class EntityStateMachine
+{
+    StateStruct allState;
+
+    public void SetState(EntityEnum _State)
+    {
+        if (allState.CurrentState != null)
+        {
+            allState.CurrentState.Exit();
+        }
+        allState.SetState(_State);
+        allState.CurrentState.Enter();
+    }
+
+    public void SetState(EntityState _State)
+    {
+        if (allState.CurrentState != null)
+        {
+            allState.CurrentState.Exit();
+        }
+        allState.SetState(_State);
+        if (allState.CurrentState != null)
+        {
+            allState.CurrentState.Enter();
+        }
+    }
+
+    public virtual void Init(StateStruct _StateStruct, Entity _Owner)
+    {
+        _StateStruct.SetOwner(_Owner);
+        _StateStruct.Init();
+        allState = _StateStruct;
+    }
+
+    public void Update()
+    {
+        if (allState.CurrentState != null)
+        {
+            allState.CurrentState.Update();
+        }
+    }
+}
---
+++ Assets/01.Scripts/Zombie/NormalZombie/NormalZombie.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalZombie : Entity
+{
+    private void Awake()
+    {
+        _stateMachine = new EntityStateMachine();
+        _stateMachine.Init(new NormalZombieStateStruct(), this);
+
+        baseStatus = new BaseStatus();
+
+        _stateMachine.SetState(EntityEnum.Run);
+    }
+}
