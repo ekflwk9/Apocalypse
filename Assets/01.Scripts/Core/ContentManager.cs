5af76fa101dd29cb43c1a6d7ff10d783c7c438b8
AmteurPotion <1k0j2i4@gmail.com>
2025-05-27 09:55:00 +0900

    [Feat] 번들관리시스템 정적 호출가능하게끔 변경

--- Assets/01.Scripts/Core/ContentManager.cs
+++ Assets/01.Scripts/Core/ContentManager.cs
@@ -2,9 +2,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Serialization;
 
 public sealed class ContentManager : MonoBehaviour
 {
+  public static ContentManager Instance { get; private set; }
+  public const string BundleLabel = "Bundle";
+  
   /// <summary>
   ///   게임중 상시 메모리에 올라가있는 데이터
   /// </summary>
@@ -13,10 +17,9 @@ public sealed class ContentManager : MonoBehaviour
   
   // 프로젝트에서 영구적으로 메모리상에 올려둘 에셋 목록
   public DataBundle sharedPackage;
-  public static ContentManager Instance { get; private set; }
   
   // 패키지의 라벨 참조입니다. 패키지를 전역적으로 불러올 때 사용합니다.
-  [SerializeField] private AssetLabelReference packageLabel;
+  [SerializeField] private AssetLabelReference bundleLabel = new() {labelString = BundleLabel};
   
   #if UNITY_EDITOR
   // 패키지를 불러왔을 시 올려놓는 메모리입니다.
@@ -35,13 +38,13 @@ public sealed class ContentManager : MonoBehaviour
     }
   }
 
-  #region PackageLoader
+  #region BundleLoader
   /// <summary>
   /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
   /// </summary>
-  /// <param name="packageName">불러올 ScenePackage의 파일명입니다.</param>
-  /// <param name="label">불러올 ScenePackage의 어드레서블 라벨입니다.</param>
-  public async Task<DataBundle> LoadPackage(string packageName, AssetLabelReference label)
+  /// <param name="bundleName">불러올 DataBundle의 파일명입니다.</param>
+  /// <param name="label">불러올 DataBundle의 어드레서블 라벨입니다.</param>
+  public static async Task<DataBundle> LoadBundle(string bundleName, AssetLabelReference label)
   {
     var result = (await Addressables.LoadAssetsAsync<DataBundle>(label, null).Task).ToArray();
     
@@ -51,26 +54,41 @@ public sealed class ContentManager : MonoBehaviour
       return null;
     }
 
-    return (from package in result where package.name == packageName select package).First();
+    return (from bundle in result where bundle.name == bundleName select bundle).First();
   }
   
   /// <summary>
   /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
   /// </summary>
-  /// <param name="packageName">불러올 ScenePackage의 파일명입니다.</param>
+  /// <param name="bundleName">불러올 DataBundle의 파일명입니다.</param>
+  /// <param name="label">불러올 DataBundle의 어드레서블 문자열 라벨입니다.</param>
   /// <returns></returns>
-  public async Task<DataBundle> LoadPackage(string packageName) => await LoadPackage(packageName, packageLabel);
+  public static async Task<DataBundle> LoadBundle(string bundleName, string label) => await LoadBundle(bundleName, new AssetLabelReference{labelString = label});
+  
+  /// <summary>
+  /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
+  /// </summary>
+  /// <param name="bundleName">불러올 DataBundle의 파일명입니다.</param>
+  /// <returns></returns>
+  public static async Task<DataBundle> LoadBundle(string bundleName)
+  {
+    AssetLabelReference label;
+
+    if (Instance) label = Instance.bundleLabel;
+    else label = new AssetLabelReference{labelString = BundleLabel};
+    return await LoadBundle(bundleName, label);
+  }
   
   /// <summary>
   /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
   /// 동기적으로 패키지데이터를 불러옵니다.
   /// </summary>
-  /// <param name="packageName">불러올 ScenePackage의 파일명입니다.</param>
-  /// <param name="label">불러올 ScenePackage의 어드레서블 라벨입니다.</param>
+  /// <param name="bundleName">불러올 DataBundle의 파일명입니다.</param>
+  /// <param name="label">불러올 DataBundle의 어드레서블 라벨입니다.</param>
   /// <returns></returns>
-  public DataBundle LoadPackageSync(string packageName, AssetLabelReference label)
+  public static DataBundle LoadBundleSync(string bundleName, AssetLabelReference label)
   {
-    var task = LoadPackage(packageName);
+    var task = LoadBundle(bundleName, label);
     task.Wait();
     return task.Result;
   }
@@ -79,9 +97,23 @@ public sealed class ContentManager : MonoBehaviour
   /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
   /// 동기적으로 패키지데이터를 불러옵니다.
   /// </summary>
-  /// <param name="packageName">불러올 ScenePackage의 파일명입니다.</param>
+  /// <param name="bundleName">불러올 ScenePackage의 파일명입니다.</param>
   /// <returns></returns>
-  public DataBundle LoadPackageSync(string packageName) => LoadPackageSync(packageName, packageLabel);
+  public static DataBundle LoadBundleSync(string bundleName)
+  {
+    var task = LoadBundle(bundleName);
+    task.Wait();
+    return task.Result;
+  }
+  
+  /// <summary>
+  /// 전체 에셋을 불러오고 다시 비활성화하기 때문에 사용을 추천하지 않습니다.
+  /// 동기적으로 패키지데이터를 불러옵니다.
+  /// </summary>
+  /// <param name="bundleName"></param>
+  /// <param name="label"></param>
+  /// <returns></returns>
+  public static DataBundle LoadBundleSync(string bundleName, string label) => LoadBundleSync(bundleName, new AssetLabelReference{labelString = label});
   #endregion PackageLoader
 
   #region Unity Events
