6c1fcd870632c39848d79877ff320349ec900aee
jk1018 <1018jkjk@gmail.com>
2025-05-28 17:24:24 +0900

    fix : ItemHandler 수정입니다

--- Assets/01.Scripts/Item/ItemSystem/ItemHandler.cs
+++ Assets/01.Scripts/Item/ItemSystem/ItemHandler.cs
@@ -6,15 +6,9 @@ using UnityEngine.AddressableAssets;
 public class ItemHandler : MonoBehaviour
 {
     public ItemInfo itemInfo;
-    private Transform playerTransform;
+    private Transform _playerTransform;
+    private Transform PlayerTransform => _playerTransform ??= Player.Instance.transform;
 
-    private void OnEnable()
-    {
-        if (playerTransform == null)
-        {
-            playerTransform = Player.Instance.transform;
-        }
-    }
 
     public void PickUpItem() // 아이템 주을때 호출 (인풋시스템 연동 예정)
     {
@@ -24,10 +18,7 @@ public class ItemHandler : MonoBehaviour
 
     public void DropItem()
     {
-        if (playerTransform == null)
-        { playerTransform = Player.Instance.transform; }
-
-        this.transform.position = playerTransform.position;
+        this.transform.position = PlayerTransform.position;
         this.gameObject.SetActive(true);
     }
 }
