37446070229fed7eeda3cd22510fd1d902762831
jk1018 <1018jkjk@gmail.com>
2025-05-28 11:53:07 +0900

    feat : 인벤토리 로직입니다

--- Assets/01.Scripts/Item/ItemScripts/ItemInfo.cs
+++ Assets/01.Scripts/Item/ItemScripts/ItemInfo.cs
@@ -1,20 +1,18 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-namespace GameItem
+public class ItemInfo : ScriptableObject
 {
-    public class ItemInfo : ScriptableObject
-    {
-        [Header("Info")]
-        public int itemId;
-        public string itemName;
-        public string disciption;
-        public ItemType itemType;
-        public Sprite icon;
-        public int cost;
+    [Header("Info")]
+    public int itemId;
+    public string itemName;
+    public string disciption;
+    public ItemType itemType;
+    public Sprite icon;
+    public int cost;
 
-        [Header("Data")]
-        public bool canStack;
-        public int maxStack;
-    }
+    [Header("Data")]
+    public bool canStack;
+    public int maxStack;
 }
+
--- Assets/01.Scripts/Item/ItemSystem/ItemManager.cs
+++ Assets/01.Scripts/Item/ItemSystem/ItemManager.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using GameItem;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
