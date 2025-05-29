c83abc72da04921d95e47f44eb930feb2bb2def1
ekflwk9 <ekflwk9@naver.com>
2025-05-29 19:59:23 +0900

    [feat] 캐릭터 창 주무기 슬롯 생성
    캐릭터 창 주무기 슬롯 생성

---
+++ Assets/01.Scripts/Ui/Status/PlaySlot.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySlot : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text countText;
+
+    private void Reset()
+    {
+        var iconPos = Helper.FindChild(this.transform, nameof(icon));
+
+        if (iconPos.TryGetComponent<TMP_Text>(out var isText)) countText = isText;
+        else DebugHelper.ShowBugWindow($"{this.name}에 가 존재하지 않음");
+    }
+
+    /// <summary>
+    /// 화면에 보이는 슬롯 
+    /// </summary>
+    /// <param name="_itemId"></param>
+    /// <param name="_count"></param>
+    public void SetSlotView(int _itemId, int _count)
+    {
+        var item = ItemManager.Instance.itemDB[_itemId];
+
+        icon.sprite = item.icon;
+        icon.color = Color.white;
+        countText.text = _count.ToString();
+    }
+
+    /// <summary>
+    /// 매개변수가 없다면 화면에 보이는 슬롯을 지워줌
+    /// </summary>
+    /// <param name="_count"></param>
+    public void SetSlotView()
+    { 
+        countText.text = "";
+        icon.color = Color.clear;
+    }
+}
