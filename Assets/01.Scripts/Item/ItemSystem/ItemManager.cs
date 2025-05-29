3f41f4a735e831b52b934032b1d6b31629954c26
jk1018 <1018jkjk@gmail.com>
2025-05-29 12:33:39 +0900

    fix : 아이템 프리팹 수정입니다

--- Assets/01.Scripts/Item/ItemSystem/ItemManager.cs
+++ Assets/01.Scripts/Item/ItemSystem/ItemManager.cs
@@ -4,40 +4,36 @@ using UnityEngine.AddressableAssets;
 
 public class ItemManager : MonoBehaviour
 {
-    [SerializeField] private AssetBundle dataBundle;
+    [SerializeField] private AssetData dataBundle;
     public Inventory _inventory;
     public Inventory Inventory => _inventory ??= Player.Instance.GetComponent<Inventory>();
     public ItemEquipment itemEquipment;
 
-    private static ItemManager _instance;
     public static ItemManager Instance
     {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = new GameObject("ItemManager").AddComponent<ItemManager>();
-            }
-            return _instance;
-        }
+        get; private set;
     }
 
     private void Awake()
     {
-        if (_instance == null)
+        if (Instance == null)
         {
-            _instance = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
 
-            Addressables.LoadAssetsAsync<ItemInfo>
-            (new AssetLabelReference() { labelString = "Item" }, item =>
+            dataBundle = ContentManager.LoadBundleSync("ItemBundle").LoadSync();
+
+            foreach (var obj in dataBundle.AllAssets)
             {
-                itemDB[item.itemId] = item;
-            }).WaitForCompletion();
+                if (obj.Value is ItemInfo itemInfo)
+                {
+                    itemDB[itemInfo.itemId] = itemInfo;
+                }
+            }
         }
         else
         {
-            if (_instance != this)
+            if (Instance != this)
             {
                 Destroy(gameObject);
             }
@@ -46,18 +42,68 @@ public class ItemManager : MonoBehaviour
 
     public void PickUp(int itemId) // 플레이어가 아이템 주웠을때 호출되는 메서드
     {
-        ItemInfo item;
         if (itemDB.ContainsKey(itemId))
         {
-            item = itemDB[itemId];
-
-            Inventory.GetItem(item);
+            if (UiManager.instance.status.GetItem(itemId))
+            {
+                Inventory.GetItem(itemDB[itemId]);
+                UiManager.instance.status.GetItem(itemId);
+            }
+        }
+        else
+        {
+            DebugHelper.Log($"{itemId}키를 가진 아이템은 없음");
         }
     }
-    // public void UseItem(ItemInfo itemInfo)
-    // {
 
-    // }
+    /// <summary>
+    /// 아이템 제거시 호출 요망
+    /// </summary>
+    /// <param name="itemId"></param>
+    public void RemoveItem(int itemId)
+    {
+        Inventory.RemoveInventoryItem(itemId);
+    }
+
+    /// <summary>
+    /// 아이템 사용시 호출 요망
+    /// </summary>
+    /// <param name="itemId"></param>
+    public void UseItem(int itemId)
+    {
+        Inventory.UseInventoryItem(itemId);
+    }
+
+    /// <summary>
+    /// 주아이템 등록시 호출 요망
+    /// </summary>
+    /// <param name="index"></param>
+    public void SetFirstItem(int index)
+    {
+        Inventory.firstSlotItem = Instance.itemDB[index];
+    }
+
+    /// <summary>
+    /// 보조아이템 등록시 호출 요망
+    /// </summary>
+    /// <param name="index"></param>
+    public void SetSecondItem(int index) // 보조무기 
+    {
+        Inventory.secondSlotItem = Instance.itemDB[index];
+    }
+
+    /// <summary>
+    /// 맵 아이템 생성 메서드
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="itemInfo"></param>
+    /// <returns></returns>
+    public ItemHandler SpawnItem(Vector3 position, ItemInfo itemInfo)
+    {
+        var Mapitem = Instantiate(itemInfo.itemPrefab, position, Quaternion.identity);
+
+        return Mapitem.GetComponent<ItemHandler>();
+    }
 
     public Dictionary<int, ItemInfo> itemDB = new Dictionary<int, ItemInfo>();
 }
