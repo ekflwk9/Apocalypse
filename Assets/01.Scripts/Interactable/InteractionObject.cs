35ad0b6c8305df0aa704e8099a7f57fdcdc80090
ekflwk9 <ekflwk9@naver.com>
2025-05-31 00:25:22 +0900

    [feat] 아이템 파밍 스크립트
    아이템 파밍 스크립트

--- Assets/01.Scripts/Interactable/InteractionObject.cs
+++ Assets/01.Scripts/Interactable/InteractionObject.cs
@@ -2,35 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public interface IInteractionObject
 {
     public void Interaction();
 }
 
+public struct FarmingData
+{
+    public int id { get; private set; }
+    public int count { get; private set; }
+    public int slotNumber { get; private set; }
+
+    public FarmingData(int _id, int _count, int _slotNumber)
+    {
+        id = _id;
+        count = _count;
+        slotNumber = _slotNumber;
+    }
+}
 
 public class InteractionObject : MonoBehaviour, IInteractionObject
 {
-    List<int> ItemKeys = new List<int>();
-
-
-    public void Awake()
-    {
-    }
-
-    public void Start()
-    {
-    }
+    List<FarmingData> ItemKeys = new List<FarmingData>();
 
     public void Interaction()
     {
         foreach (var item in ItemKeys)
         {
-            UiManager.instance.status.GetFarmingItem(item);
+            UiManager.instance.status.GetFarmingItem(item.id);
         }
+
         UiManager.instance.status.farming.gameObject.SetActive(true);
         UiManager.instance.status.inventory.gameObject.SetActive(true);
         UiManager.instance.status.equipped.gameObject.SetActive(true);
     }
 
+    public void UpdateData()
+    {
+
+    }
 }
