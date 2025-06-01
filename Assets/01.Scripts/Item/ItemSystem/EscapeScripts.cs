0e3be16233bde67884eb758810f27e5282fee525
성민 유 <usm9401@gmail.com>
2025-06-01 21:22:29 +0900

    [feat]Escape Interactionable Object

--- Assets/01.Scripts/Item/ItemSystem/EscapeScripts.cs
+++ Assets/01.Scripts/Item/ItemSystem/EscapeScripts.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class EscapeScripts : MonoBehaviour, IInteractionObject
 {
+
+    Collider _collider;
+
+    private void Reset()
+    {
+        _collider = GetComponent<Collider>();
+
+        gameObject.tag = TagHelper.Item;
+        gameObject.layer = LayerHelper.InitLayer(LayerHelper.Item);
+    }
+
     public void Interaction()
     {
         UiManager.instance.status.success.gameObject.SetActive(true);
